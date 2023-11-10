a0a41c577d525f3a9cc682c13276f297cef8df73
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-11-09 23:05:18 -0300

    Props header

--- cupcakes/src/components/headers/header.tsx
+++ cupcakes/src/components/headers/header.tsx
@@ -1,6 +1,13 @@
-export function Header({ children }: { children: React.ReactNode }) {
+interface IHeaderProps extends React.HTMLAttributes<HTMLElement> {
+  children: React.ReactNode
+}
+
+export function Header({ children, ...rest }: IHeaderProps) {
   return (
-    <header className="fixed z-auto w-full bg-primary-foreground px-2 py-2 top-0 left-0 flex items-center justify-between shadow-lg drop-shadow-lg">
+    <header
+      className="fixed z-auto w-full bg-primary-foreground px-2 py-2 top-0 left-0 flex items-center justify-between shadow-lg drop-shadow-lg"
+      {...rest}
+    >
       <div className="container flex items-center justify-between w-full">
         {children}
       </div>
