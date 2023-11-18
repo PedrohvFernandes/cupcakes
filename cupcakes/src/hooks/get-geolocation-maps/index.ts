056669c7036cd8d13841929fb4416ddfc9c703d3
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-11-17 23:26:22 -0300

    Update index.ts

--- cupcakes/src/hooks/get-geolocation-maps/index.ts
+++ cupcakes/src/hooks/get-geolocation-maps/index.ts
@@ -138,6 +138,166 @@ const getLocation = () => {
   }
 }
 
+// const getLocation = () => {
+//   if (navigator.geolocation) {
+//     let watchId: number | null = null;
+
+//     const responseState = navigator.permissions
+//       .query({ name: 'geolocation' })
+//       .then((response): Promise<IGeolocationPosition> => {
+//         const responseData = {
+//           responseState: {
+//             responseState: response.state
+//           },
+//           responseDataMap: {
+//             center: { lat: 0, lng: 0 },
+//             accuracy: 0
+//           },
+//           error: { code: 0, message: '' },
+//           messageGeolocationNotSupportedBrowser: { error: '' },
+//           cleanup: () => {
+//             // Limpe a assinatura quando não for mais necessário (por exemplo, componente desmontado)
+//             if (watchId !== null) {
+//               navigator.geolocation.clearWatch(watchId);
+//             }
+//           }
+//         };
+
+//         watchId = navigator.geolocation.watchPosition(
+//           position => {
+//             responseData.responseDataMap.center = {
+//               lat: success(position).latitude,
+//               lng: success(position).longitude
+//             };
+//             responseData.responseDataMap.accuracy = success(position).accuracy;
+//           },
+//           error => {
+//             responseData.responseDataMap.center = { lat: 0, lng: 0 };
+//             responseData.responseDataMap.accuracy = 0;
+//             responseData.error = {
+//               code: errors(error).errorCode,
+//               message: errors(error).errorMessage
+//             };
+//           },
+//           OPTIONS
+//         );
+
+//         return Promise.resolve(responseData);
+//       });
+
+//     return responseState;
+//   } else {
+//     return new Promise<IGeolocationPosition>(resolve => {
+//       navigator.geolocation.watchPosition(
+//         () =>
+//           resolve({
+//             messageGeolocationNotSupportedBrowser: {
+//               error: 'A geolocalização não é suportada por este navegador.'
+//             }
+//           }),
+//         () =>
+//           resolve({
+//             messageGeolocationNotSupportedBrowser: {
+//               error: 'A geolocalização não é suportada por este navegador.'
+//             }
+//           }),
+//         OPTIONS
+//       );
+//     });
+//   }
+// };
+
+// const getLocation = () => {
+//   if (navigator.geolocation) {
+//     let watchId: number | null = null;
+    
+//     const responseState = navigator.permissions
+//       .query({ name: 'geolocation' })
+//       .then((response): Promise<IGeolocationPosition> => {
+//         return new Promise<IGeolocationPosition>((resolve, reject) => {
+//           const timeoutId = setTimeout(() => {
+//             reject({
+//               responseState: { responseState: response.state },
+//               responseDataMap: { center: { lat: 0, lng: 0 }, accuracy: 0 },
+//               error: { code: 0, message: 'Timeout ao obter a localização.' },
+//               messageGeolocationNotSupportedBrowser: { error: '' },
+//               cleanup: () => {
+//                 if (watchId !== null) {
+//                   navigator.geolocation.clearWatch(watchId);
+//                 }
+//               }
+//             });
+//           }, 10000); // 10 segundos timeout (ajuste conforme necessário)
+
+//           watchId = navigator.geolocation.watchPosition(
+//             position => {
+//               const accuracy = position.coords.accuracy || 0;
+
+//               // Ajuste a precisão desejada conforme necessário
+//               if (accuracy <= 50) {
+//                 clearTimeout(timeoutId); // Limpa o timeout, pois alcançou a precisão desejada
+//                 resolve({
+//                   responseState: { responseState: response.state },
+//                   responseDataMap: {
+//                     center: {
+//                       lat: success(position).latitude,
+//                       lng: success(position).longitude
+//                     },
+//                     accuracy: success(position).accuracy
+//                   },
+//                   error: { code: 0, message: '' },
+//                   messageGeolocationNotSupportedBrowser: { error: '' },
+//                   cleanup: () => {
+//                     if (watchId !== null) {
+//                       navigator.geolocation.clearWatch(watchId);
+//                     }
+//                   }
+//                 });
+//               }
+//             },
+//             error => {
+//               reject({
+//                 responseState: { responseState: response.state },
+//                 responseDataMap: { center: { lat: 0, lng: 0 }, accuracy: 0 },
+//                 error: {
+//                   code: errors(error).errorCode,
+//                   message: errors(error).errorMessage
+//                 },
+//                 messageGeolocationNotSupportedBrowser: { error: '' },
+//                 cleanup: () => {
+//                   if (watchId !== null) {
+//                     navigator.geolocation.clearWatch(watchId);
+//                   }
+//                 }
+//               });
+//             },
+//             OPTIONS
+//           );
+//         });
+//       });
+
+//     return responseState;
+//   } else {
+//     return new Promise<IGeolocationPosition>(resolve => {
+//       navigator.geolocation.watchPosition(
+//         () =>
+//           resolve({
+//             messageGeolocationNotSupportedBrowser: {
+//               error: 'A geolocalização não é suportada por este navegador.'
+//             }
+//           }),
+//         () =>
+//           resolve({
+//             messageGeolocationNotSupportedBrowser: {
+//               error: 'A geolocalização não é suportada por este navegador.'
+//             }
+//           }),
+//         OPTIONS
+//       );
+//     });
+//   }
+// };
+
 const useGetGeolocationMaps = () => {
   return {
     OPTIONS_MAP,
