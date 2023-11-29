5ef0ffa0c2cfb0c493fb197da01bcd2a618fcadc
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-11-29 11:31:55 -0300

    Keys Stripe

--- cupcakes/src/config/auth.ts
+++ cupcakes/src/config/auth.ts
@@ -7,6 +7,16 @@ export default {
           key: import.meta.env.VITE_GOOGLE_CLOUD_MAPS_CUPCAKES_API_KEY
         }
       }
+    },
+    stripe: {
+      keys: {
+        private: {
+          key: import.meta.env.VITE_STRIPE_CUPCAKES_API_PRIVATE_KEY
+        },
+        public: {
+          key: import.meta.env.VITE_STRIPE_CUPCAKES_API_PUBLIC_KEY
+        }
+      }
     }
   }
 }
