2a94f03648c6849464808c6605d4adde7b80eb95
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-11-30 03:39:37 -0300

    dist

--- api/dist/router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const controller_product_1 = require("@infra/controller/controller-product");
-const express_1 = require("express");
-exports.routes = (0, express_1.Router)();
-exports.routes.get('/products-all', new controller_product_1.ControllerProducts().GetAllProductsController);
