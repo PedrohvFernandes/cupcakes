25b8b35a7f05e61dd94a763544208c3f31aab3b6
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-11-30 21:10:26 -0300

    Testando husky

--- api/dist/infra/error/product-not-found.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProductNotFound = void 0;
-class ProductNotFound extends Error {
-    constructor() {
-        super('Product not found');
-    }
-}
-exports.ProductNotFound = ProductNotFound;
