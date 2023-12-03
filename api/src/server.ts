73c6289d7b7491a906bad91baebe4b4685cd551a
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-12-02 22:13:44 -0300

    Preparando ambiente

--- api/src/server.ts
+++ api/src/server.ts
@@ -7,7 +7,7 @@ import { routes } from './router'
 // PORT = A porta que a plataforma(vercel) de deploy vai oferecer --> colocar ela so na parte de produção da plataforma
 const PORT = process.env.PORT ?? 3333
 // PRODORDEV = seudominion.com --> colocar ela so na parte de produção da plataforma(vercel)
-const PRODORDEV = process.env.PRODORDEV ?? 'http://localhost:3000'
+const PRODORDEV = process.env.PRODORDEV ?? 'http://localhost:5173'
 
 const app = express()
 
--- cupcakes/src/config/bases.ts
+++ cupcakes/src/config/bases.ts
@@ -1,7 +1,11 @@
+const isDev = process.env.REACT_APP_ENVIRONMENT === 'development'
+
 export default {
   cupcakes: {
     baseUrls: {
-      
+      apiProducts: isDev
+        ? import.meta.env.VITE_API_PRODUCTS_CUPCAKES_URL_TEST as string
+        : import.meta.env.VITE_API_PRODUCTS_CUPCAKES_URL_LIVE as string,
     },
     gitHub: {
       baseUrls: {
@@ -10,23 +14,31 @@ export default {
       },
       target: {
         blank: '_blank',
-        parent: '_parent',
+        parent: '_parent'
       }
     },
     google: {
       baseUrls: {
-        androidDeviceLocationSettings: ' https://support.google.com/accounts/answer/3467281?hl=pt-BR',
-        appleDeviceLocationSettings: 'https://support.apple.com/pt-br/guide/iphone/iph3dd5f9be/ios#:~:text=Acesse%20Ajustes%20>%20Privacidade%20e%20Segurança%20>%20Serviços%20de%20Localização.',
-        pcLocationSettings: 'https://support.google.com/chrome/answer/142065?co=GENIE.Platform%3DDesktop&hl=pt-BR',
-        stepByStepToEnableLocation: 'https://www.ailos.coop.br/wp-content/uploads/2023/01/Passo-a-passo-HabilitacaoLocalizacaoCO-Ailos.pdf',
-        clearCacheCookiesChrome: 'https://support.google.com/accounts/answer/32050?hl=pt-BR&co=GENIE.Platform%3DAndroid',
-        clearCacheCookiesFirefox: 'https://support.mozilla.org/pt-BR/kb/limpe-cookies-e-dados-de-sites-no-firefox',
-        clearCacheCookiesFirefoxAndroid: 'https://support.mozilla.org/pt-BR/kb/limpe-historico-de-pesquisa-firefox-android',
-        clearCacheFirefox: 'https://support.mozilla.org/pt-BR/kb/como-limpar-cache-firefox'
+        androidDeviceLocationSettings:
+          ' https://support.google.com/accounts/answer/3467281?hl=pt-BR',
+        appleDeviceLocationSettings:
+          'https://support.apple.com/pt-br/guide/iphone/iph3dd5f9be/ios#:~:text=Acesse%20Ajustes%20>%20Privacidade%20e%20Segurança%20>%20Serviços%20de%20Localização.',
+        pcLocationSettings:
+          'https://support.google.com/chrome/answer/142065?co=GENIE.Platform%3DDesktop&hl=pt-BR',
+        stepByStepToEnableLocation:
+          'https://www.ailos.coop.br/wp-content/uploads/2023/01/Passo-a-passo-HabilitacaoLocalizacaoCO-Ailos.pdf',
+        clearCacheCookiesChrome:
+          'https://support.google.com/accounts/answer/32050?hl=pt-BR&co=GENIE.Platform%3DAndroid',
+        clearCacheCookiesFirefox:
+          'https://support.mozilla.org/pt-BR/kb/limpe-cookies-e-dados-de-sites-no-firefox',
+        clearCacheCookiesFirefoxAndroid:
+          'https://support.mozilla.org/pt-BR/kb/limpe-historico-de-pesquisa-firefox-android',
+        clearCacheFirefox:
+          'https://support.mozilla.org/pt-BR/kb/como-limpar-cache-firefox'
       },
       target: {
         blank: '_blank',
-        parent: '_parent',
+        parent: '_parent'
       }
     }
   }
