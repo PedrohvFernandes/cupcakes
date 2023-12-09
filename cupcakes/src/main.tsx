046e1b4d14800f3259b973511e3a3129d5c5d01a
Pedro Henrique Vieira Fernandes <72428350+PedrohvFernandes@users.noreply.github.com>
2023-12-09 15:33:07 -0300

    Update main.tsx

--- cupcakes/src/main.tsx
+++ cupcakes/src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './main.css'
 
+const isDev = import.meta.env.VITE_ENVIRONMENT === 'development'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
+  isDev ? (
     <App />
-  // </React.StrictMode>
+  ) : (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 )
